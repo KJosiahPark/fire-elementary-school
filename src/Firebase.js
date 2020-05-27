1c0cc6d5d8d47f9b1540bed028cb19566d56340e
Kyungjin Josiah Park <KyungjinJPark@gmail.com>
2020-05-27 15:27:56 -0400

    firebase connected

---
+++ src/Firebase.js
@@ -0,0 +1,45 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+ 
+const config = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+ 
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+ 
+    this.auth = app.auth();
+    this.db = app.database();
+  }
+ 
+  // // *** Auth API ***
+ 
+  // doCreateUserWithEmailAndPassword = (email, password) =>
+  //   this.auth.createUserWithEmailAndPassword(email, password);
+ 
+  // doSignInWithEmailAndPassword = (email, password) =>
+  //   this.auth.signInWithEmailAndPassword(email, password);
+ 
+  // doSignOut = () => this.auth.signOut();
+ 
+  // doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+ 
+  // doPasswordUpdate = password =>
+  //   this.auth.currentUser.updatePassword(password);
+ 
+  // // *** User API ***
+ 
+  // user = uid => this.db.ref(`users/${uid}`);
+ 
+  // users = () => this.db.ref('users');
+}
+ 
+export default Firebase;
