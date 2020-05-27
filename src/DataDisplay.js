9e02f4146b94b2321242fbd86500812504262924
KyungjinJPark <kjparkcontact@gmail.com>
2020-05-27 15:05:29 -0400

    stubbed data display until data -> firebase completed

---
+++ src/DataDisplay.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class DataDisplay extends React.Component {
+  render = () => {
+    return (
+      <div>
+        <h1>Class List</h1>
+        <h1>Teacher List</h1>
+        <h1>Student List</h1>
+      </div>
+    )
+  }
+}
+
+export default DataDisplay;
