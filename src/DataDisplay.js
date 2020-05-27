abb56f19306007aad66f79c4ab59f80a9b0914a1
KyungjinJPark <kjparkcontact@gmail.com>
2020-05-27 16:10:04 -0400

    data displays properly

--- src/DataDisplay.js
+++ src/DataDisplay.js
@@ -1,12 +1,44 @@
 import React from 'react';
 
+import Controller from './controller'
+
 class DataDisplay extends React.Component {
+  state = {
+    classes: {},
+    teachers: {},
+    students: {}
+  }
+
+  onValue = (dataGroup, val) => {
+    this.setState((prevState) => {
+      prevState[dataGroup] = val;
+      return prevState;
+    })
+  }
+
+  componentDidMount = () => {
+    Controller.setUpOnValue(this.onValue);
+  }
+
   render = () => {
     return (
       <div>
-        <h1>Class List</h1>
-        <h1>Teacher List</h1>
-        <h1>Student List</h1>
+        <button onClick={() => console.log(this.state)}>print state</button>
+        <h1>Classes List</h1>
+          {Object.keys(this.state.classes).map((key, index) => {
+            const classListing = this.state.classes[key];
+            return <p>{classListing.name}</p>;
+          })}
+        <h1>Teachers List</h1>
+          {Object.keys(this.state.teachers).map((key, index) => {
+            const teacher = this.state.teachers[key];
+            return <p>{teacher.lastName}</p>;
+          })}
+        <h1>Students List</h1>
+          {Object.keys(this.state.students).map((key, index) => {
+            const student = this.state.students[key];
+            return <p>{student.lastName}</p>;
+          })}
       </div>
     )
   }
--- src/controller.js
+++ src/controller.js
@@ -2,10 +2,28 @@ import Firebase from './Firebase'
 
 class Controller {
   static fireb = new Firebase();
+
+  static setUpOnValue = (onValueFunc) => {
+    Controller.fireb.db.ref('root/school-data/classes/').on('value',
+    (snapshot) => {
+      onValueFunc("classes", snapshot.val());
+    },
+    (err) => console.log(err));
+    Controller.fireb.db.ref('root/school-data/teachers/').on('value',
+    (snapshot) => {
+      onValueFunc("teachers", snapshot.val());
+    },
+    (err) => console.log(err));
+    Controller.fireb.db.ref('root/school-data/students/').on('value',
+    (snapshot) => {
+      onValueFunc("students", snapshot.val());
+    },
+    (err) => console.log(err));
+  }
   
   static registerClass = (className) => {
     console.log(`registering class: ${className} ...`);
-    Controller.fireb.db.ref("root/school-data/classes").push({name: className});
+    Controller.fireb.db.ref("root/school-data/classes/").push({name: className});
   }
 
   static registerTeacher = (lastName, firstName) => {
@@ -14,7 +32,7 @@ class Controller {
       lastName: lastName,
       firstName: firstName
     }
-    Controller.fireb.db.ref("root/school-data/teachers").push(teacher);
+    Controller.fireb.db.ref("root/school-data/teachers/").push(teacher);
   }
 
   static registerStudent = (lastName, firstName) => {
@@ -23,7 +41,7 @@ class Controller {
       lastName: lastName,
       firstName: firstName
     }
-    Controller.fireb.db.ref("root/school-data/students").push(student);
+    Controller.fireb.db.ref("root/school-data/students/").push(student);
   }
 }
 
